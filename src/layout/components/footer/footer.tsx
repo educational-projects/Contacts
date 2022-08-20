b9f8a9f4b2cadd91c4fade19f424f88b56347780
ваше имя <dimarik-tixonov@yandex.ru>
2022-08-20 19:48:44 +0300

    add: добавит футер

--- src/layout/components/footer/footer.tsx
+++ src/layout/components/footer/footer.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import styles from './footer.module.scss';
 
 export function Footer(): JSX.Element {
+  const currentYear = new Date().getFullYear();
   return (
-    <footer>
-      Футер приложения
+    <footer className={styles.footer}>
+      <div className="container">
+        <div className={styles.copyright}>
+          <p>
+            ©
+            {' '}
+            {currentYear}
+            {' '}
+            limestormrage
+          </p>
+        </div>
+      </div>
     </footer>
   );
 }
