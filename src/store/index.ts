960a897c83b82096b4331dbc877202ed45a5216a
ваше имя <dimarik-tixonov@yandex.ru>
2022-08-30 21:04:20 +0300

    add: подключит апи

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,18 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { createAPI } from '../services/api';
 import userReducer from './user/user';
+
+const api = createAPI();
 
 const store = configureStore({
   reducer: {
     user: userReducer,
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+    thunk: {
+      extraArgument: api,
+    },
+  }),
 });
 
 export default store;
